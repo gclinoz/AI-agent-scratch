d9cc412dd6a4d5a7026af7c642f6d31ab83bce03
gclinoz <t96994024@gs.ncku.edu.tw>
2026-03-26 11:08:43 +0800

    Ch1-L5: User Input

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="Enter user prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,16 +14,14 @@ if api_key == None:
     raise RuntimeError("API key not found")
 
 client = genai.Client(api_key=api_key)
-
-contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=contents
+    model='gemini-2.5-flash', contents=args.user_prompt
 )
 
 if response.usage_metadata == None:
     raise RuntimeError("API request fail")
 
-print(f"User prompt: {contents}")
+print(f"User prompt: {args.user_prompt}")
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Response:\n{response.text}")
