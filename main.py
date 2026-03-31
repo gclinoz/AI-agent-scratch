1fb1651f93d7e45c7e1a19622d1261f31152e35c
gclinoz <t96994024@gs.ncku.edu.tw>
2026-03-31 15:24:19 +0800

    Ch3-L2: Function Declaration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 import argparse
 
 parser = argparse.ArgumentParser(description="Chatbot")
@@ -26,7 +27,8 @@ messages = [types.Content(
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages,
     config=types.GenerateContentConfig(
-        system_instruction=system_prompt, temperature=0
+        system_instruction=system_prompt,
+        tools=[available_functions]
     )
 )
 
@@ -34,9 +36,20 @@ if response.usage_metadata == None:
     raise RuntimeError("API request fail")
 
 if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    if response.function_calls == None:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response:\n{response.text}")
+    else:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        for fn in  response.function_calls:
+            print(f"Calling function: {fn.name}({fn.args})")
 
-print(f"Response:\n{response.text}")
+if response.function_calls == None:
+    print(f"Response:\n{response.text}")
+else:
+    for fn in  response.function_calls:
+        print(f"Calling function: {fn.name}({fn.args})")
